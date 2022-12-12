9e846ca3dd3cab9be6f42695121505017d4ed9fe
YannickDurindel <86116680+YannickDurindel@users.noreply.github.com>
2022-12-12 13:03:21 +0100

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -21,19 +21,12 @@ Created on Mon Dec 12 12:19:43 2022
 with open('/home/yannick/Documents/useless/py_codes/Email_broadcast/email_list.txt') as f:
     email = [line for line in f.readlines()]
 
-user = 'yannick.durindel@gmail.com'
-app_password = 'vavwcwykkzbmpdvw' # a token for gmail
-subject = 'Can you teach your computer how to trade crypto 24/7 ?'
+user = 'your@email'
+app_password = 'delivered by google account' # a token for gmail
+subject = 'your subject'
 
 content = """
-Can You teach your computer how to trade cryptocurrencies 24/7 ?
-
-The answer is yes ! And guess what, you don't even need to know about trading, or coding !!!
-If you want more info check this website : http://cryptobot.trading , you'll get a free insight.
-
-Hopping you'll enjoy !
-Durindel Yannick
-Follow me on instagram at @student_entreprneur
+your email
 """
 
 for k in range(96,len(email)):
